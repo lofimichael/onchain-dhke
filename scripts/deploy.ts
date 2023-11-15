bd95e118e4bd2e43ec5ff67b8dbb7183959ab647
Michael Kirsanov <94795502+lofimichael@users.noreply.github.com>
2023-11-15 00:41:23 -0400

    hardhat added (WIP)

---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+// deploy script
+
+import { ethers } from 'ethers'
+import { Contract, ContractFactory } from "ethers";
+
+// const main = async () => {
+
+//     // deploy the contracts in ./src
+//     const [deployer] = await ethers.getSigners();
+//     console.log(`Deploying contracts with the account: ${deployer.address}`);
+
+//     // deploy the contracts
+//     const Token: ContractFactory = await ethers.getContractFactory("Token");
+//     const token: Contract = await Token.deploy();
+//     console.log(`Token address: ${token.address}`);
+
+
+
+// }
